0b2cd54f9d13e0e2b1613e8ca36c609573d235b2
Vladislav Kuznecov <fizvlad@mail.ru>
2021-03-06 15:21:07 +0300

    feat: add substitutions for win-format paths

--- src/resolver.ts
+++ src/resolver.ts
@@ -13,43 +13,53 @@ export function isSpec(file) {
 export function codeToSpec(file) {
 	var viewRegex = /erb$|haml$|slim$/
 	var isViewFile = file.match(viewRegex);
-	
+
 	if (isViewFile) {
 		return file
 			.replace('/app/', '/spec/')
+			.replace('\\app\\', '\\spec\\')
 			.replace('.haml', '.haml_spec.rb')
 			.replace('.erb', '.erb_spec.rb')
 			.replace('.slim', '.slim_spec.rb');
 	}
-	
+
 	file = file.replace('.rb', '_spec.rb');
-	
+
 	var isLibFile = file.indexOf('/lib/') > -1;
 	if (isLibFile) {
-		return file.replace('/lib/', '/spec/lib/');
+		return file
+			.replace('/lib/', '/spec/lib/')
+			.replace('\\lib\\', '\\spec\\lib\\');
 	}
-	
-	return file.replace('/app/', '/spec/');
+
+	return file
+		.replace('/app/', '/spec/')
+		.replace('\\app\\', '\\spec\\');
 }
 
 export function specToCode(file: string) {
 
 	var viewRegex = /(.erb|.haml|.slim)_spec.rb$/;
-	
+
 	var isViewFile = file.match(viewRegex);
 	if (isViewFile) {
 		return file
 			.replace('_spec.rb', '')
-			.replace('/spec', '/app');
+			.replace('/spec', '/app')
+			.replace('\\spec', '\\app');
 	}
 
 	file = file.replace('_spec.rb', '.rb');
 
 	var isLibFile = file.indexOf('/spec/lib/') > -1;
 	if (isLibFile) {
-		return file.replace('/spec/lib/', '/lib/');
+		return file
+			.replace('/spec/lib/', '/lib/')
+			.replace('\\spec\\lib\\', '\\lib\\');
 	}
 
-	return file.replace('/spec/', '/app/');
+	return file
+		.replace('/spec/', '/app/')
+		.replace('\\spec\\', '\\app\\');
 }
 
